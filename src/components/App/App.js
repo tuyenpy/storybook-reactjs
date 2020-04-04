701870032fa085dbf2867c0f4fef708a09b488a5
songthinhvuong87 <songthinhvuong87@gmail.com>
2020-04-04 10:34:26 +0700

    update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import {
+  BrowserRouter
+} from 'react-router-dom';
 // import logo from './logo.svg';
 // import './App.css';
+import IconBar from '../Menu/IconBar';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <IconBar />
+      </div>
+    </BrowserRouter>
   );
 }
 
